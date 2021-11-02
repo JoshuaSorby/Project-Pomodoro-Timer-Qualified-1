2848be2539c9efc7e1eacbb9f56e6fa949628d7f
Joshua Sorby <joshuassorby@gmail.com>
2021-11-02 12:07:43 -0500

    Updates

---
+++ precache-manifest.fafd38ce15b94764d8070537b666393a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d5d41dd675bbd07599f1aa1dbe9a1e",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "d7b80ee847b6104699a4",
+    "url": "/pomodoro-timer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/pomodoro-timer/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro-timer/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b80ee847b6104699a4",
+    "url": "/pomodoro-timer/static/js/main.eae6e471.chunk.js"
+  },
+  {
+    "revision": "5b934f73dc3f50a7b525",
+    "url": "/pomodoro-timer/static/js/runtime-main.cb3a558a.js"
+  }
+]);
